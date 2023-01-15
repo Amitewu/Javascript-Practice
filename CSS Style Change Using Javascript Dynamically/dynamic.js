3863c1ae5f34242d164400ab00225cb0d1fff14b
Amit Roy <ramit8339@gmail.com>
2023-01-16 00:30:35 +0600

    CSS Style Change Using Javascript Dynamically

---
+++ CSS Style Change Using Javascript Dynamically/dynamic.js
@@ -0,0 +1,9 @@
+var myStyle=document.querySelector("#para-id");
+
+function addStyle(){
+    myStyle.classList.add("para-style");
+}
+
+function remStyle(){
+    myStyle.classList.remove("para-style");
+}
